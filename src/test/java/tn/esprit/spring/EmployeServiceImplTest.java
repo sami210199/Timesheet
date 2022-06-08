4eee82fc45d440b85478c872bd6d78404af5f7db
Khalil <khalil.trojette@esprit.tn>
2022-06-08 13:09:36 +0100

    sonar fix after merge

--- src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
@@ -1,5 +1,7 @@
 package tn.esprit.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,11 +10,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Role;
+import tn.esprit.spring.services.EmployeServiceImpl;
 import tn.esprit.spring.services.IEmployeService;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class EmployeServiceImplTest {
+
+    private static final Logger L = LoggerFactory.getLogger(EmployeServiceImpl.class);
 
     @Test
     public void contextLoads() {
@@ -25,11 +30,15 @@ public class EmployeServiceImplTest {
 
     @Test
     public void testaddOrUpdateEmploye() {
+        L.info("Start Test method testaddOrUpdateEmploye()");
         Employe employe = new Employe("trojette","khalil","khalil.trojette@esprit.com","123456",true, Role.TECHNICIEN);
         int nmbrEmployeeBefore = iEmployeService.getNombreEmployeJPQL();
         int savedEmployeId =  iEmployeService.addOrUpdateEmploye(employe);
         int nmbrEmployeeAfter = iEmployeService.getNombreEmployeJPQL();
+        L.debug("asserting that employee nmbr after add is the same nmbr +1 ");
+
         Assert.assertEquals(nmbrEmployeeBefore,nmbrEmployeeAfter-1);
+        L.debug("delete entity created for test ");
         iEmployeService.deleteEmployeById(savedEmployeId);
 
     }
